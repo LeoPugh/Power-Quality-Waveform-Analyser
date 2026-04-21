f90d9686de4ae77efc666e1bbd232e01fdb0b209
Leo <leo2.pugh@live.uwe.ac.uk>
2026-04-21 15:30:37 +0100

    Declaring functions for the file io.

--- io.h
+++ io.h
@@ -3,4 +3,10 @@
 #ifndef POWER_QUALITY_WAVEFORM_ANALYSER_IO_H
 #define POWER_QUALITY_WAVEFORM_ANALYSER_IO_H
 
+#include "waveform.h"
+
+int count_rows(char *filename);
+
+struct WaveformSample *read_log(char *filename, int *rows);
+
 #endif //POWER_QUALITY_WAVEFORM_ANALYSER_IO_H
