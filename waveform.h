e85648fcebb91546f443f3ab732239130085af7d
Leo <leo2.pugh@live.uwe.ac.uk>
2026-04-21 15:36:14 +0100

    The struct being used.

--- waveform.h
+++ waveform.h
@@ -3,4 +3,17 @@
 #ifndef POWER_QUALITY_WAVEFORM_ANALYSER_WAVEFORM_H
 #define POWER_QUALITY_WAVEFORM_ANALYSER_WAVEFORM_H
 
+struct WaveformSample {
+    // Creating the fields for each sample
+    double timestamp;
+    double phase_A_voltage;
+    double phase_B_voltage;
+    double phase_C_voltage;
+    double line_current;
+    double frequency;
+    double power_factor;
+    double thd_percent;
+    // Using double as it has a higher length limit and can include decimal values
+};
+
 #endif //POWER_QUALITY_WAVEFORM_ANALYSER_WAVEFORM_H
