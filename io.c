07548c6ffac484eb5dbd9c17042ed23211e9f6ea
Leo <leo2.pugh@live.uwe.ac.uk>
2026-04-21 15:35:34 +0100

    Defining functions for counting rows of file then parsing values of file into a struct.

--- io.c
+++ io.c
@@ -1,3 +1,79 @@
 
 
 #include "io.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+int count_rows(char *filename) {
+    FILE *filepointer = fopen(filename, "r");
+    if (filepointer == NULL) { //Making sure file can be read
+        printf("Cannot open file.");
+        return 1;
+    }
+
+    int count = 0;
+    char line[256];
+
+    fgets(line, sizeof(line), filepointer);
+
+    while (fgets(line, sizeof(line), filepointer) != NULL) {
+        count++; //Count is number of rows. Increases until there are no more rows.
+    }
+
+    fclose(filepointer);
+    return count;
+}
+
+
+struct WaveformSample *read_log(char *filename, int *rows) {
+    *rows = count_rows(filename);
+
+    struct WaveformSample *array = malloc(*rows * sizeof(struct WaveformSample)); //Malloc for memory allocation
+
+    FILE *filepointer = fopen(filename, "r");
+    if (filepointer == NULL) { //Making sure file can be read
+        printf("Cannot open file.");
+        return NULL;
+    }
+
+    int count = 0;
+    char line[256];
+
+    fgets(line, sizeof(line), filepointer);
+
+    while (fgets(line, sizeof(line), filepointer) != NULL) { //Goes until end of file
+
+        char *token;
+
+        token = strtok(line, ","); //Creating token which is separated by commas
+        array[count].timestamp = atof(token); //Placing the current tokens value into the struct
+
+        token = strtok(NULL, ","); //NULL used to continue reading same string
+        array[count].phase_A_voltage = atof(token);
+
+        token = strtok(NULL, ",");
+        array[count].phase_B_voltage = atof(token);
+
+        token = strtok(NULL, ",");
+        array[count].phase_C_voltage = atof(token);
+
+        token = strtok(NULL, ",");
+        array[count].line_current = atof(token);
+
+        token = strtok(NULL, ",");
+        array[count].frequency = atof(token);
+
+        token = strtok(NULL, ",");
+        array[count].power_factor = atof(token);
+
+        token = strtok(NULL, ",");
+        array[count].thd_percent = atof(token);
+
+        count++;
+    }
+
+    fclose(filepointer);
+    return array;
+}
