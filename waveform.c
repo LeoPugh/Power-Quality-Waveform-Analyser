5c5d3f43f62b4fc7291b4ae13a4e224ed26e6ba6
Leo <leo2.pugh@live.uwe.ac.uk>
2026-04-27 16:50:15 +0100

    Function to sort csv based on voltage magnitude then writes results to new file.

--- waveform.c
+++ waveform.c
@@ -68,3 +68,72 @@ double peak2peak(struct WaveformSample *array, int size) {
 
     return p2p;
 }
+
+void cocktail_sort(struct WaveformSample *array, int size, char *filename) {
+    int swapped = 1;
+    int start = 0;
+    int end = size - 1;
+
+    while (swapped) {
+        swapped = 0;
+
+        // forward pass
+        for (int i = start; i < end; i++) {
+            if (array[i].phase_A_voltage > array[i + 1].phase_A_voltage) {
+                struct WaveformSample *a = &array[i];
+                struct WaveformSample *b = &array[i + 1];
+                struct WaveformSample temp = *a;
+                *a = *b; // uses pointers to swap values
+                *b = temp;
+                swapped = 1;
+            }
+        }
+
+        if (!swapped) {
+            break;
+        }
+
+        swapped = 0;
+        end--;
+
+        // backward pass
+        for (int i = end - 1; i >= start; i--) {
+            if (array[i].phase_A_voltage > array[i + 1].phase_A_voltage) {
+                struct WaveformSample *a = &array[i];
+                struct WaveformSample *b = &array[i + 1];
+                struct WaveformSample temp = *a;
+                *a = *b;
+                *b = temp;
+                swapped = 1;
+            }
+        }
+
+        start++;
+    }
+
+    char sorted_file[256];
+    sprintf(sorted_file, "%s_sorted.csv", filename); // creating name of new file
+
+    FILE *fp = fopen(sorted_file, "w"); // making/changing the sorted file
+    if (fp == NULL) {
+        printf("Could not open %s.\n", sorted_file);
+        return;
+    }
+
+    fprintf(fp, "timestamp,phase_A_voltage,phase_B_voltage,phase_C_voltage,line_current,frequency,power_factor,thd_percent\n");
+    // ^write in header^
+
+    for (int i = 0; i < size; i++) { // write in new values after sort
+        fprintf(fp, "%f,%f,%f,%f,%f,%f,%f,%f\n",
+            array[i].timestamp,
+            array[i].phase_A_voltage,
+            array[i].phase_B_voltage,
+            array[i].phase_C_voltage,
+            array[i].line_current,
+            array[i].frequency,
+            array[i].power_factor,
+            array[i].thd_percent);
+    }
+
+    fclose(fp);
+}
